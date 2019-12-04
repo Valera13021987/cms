ed2f9eb64b3dc523ed335762d89758f95b754863
valera.mishin <valera13021987@gmail.com>
2019-12-04 19:28:39 +0200

    реализован middleware проверяющий существуют ли категории перед тем как создать пост

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -10,6 +10,12 @@ use Illuminate\Support\Facades\Storage;
 
 class PostsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('verifyCategoriesCount')->only(['create', 'store']);
+    }
+
+
     /**
      * Display a listing of the resource.
      *
