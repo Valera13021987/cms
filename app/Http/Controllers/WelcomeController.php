c70876c6e859deaa6b7206217d746fbbf7673fa3
valera.mishin <valera13021987@gmail.com>
2019-12-10 23:35:33 +0200

    реализован поиск

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -11,9 +11,16 @@ class WelcomeController extends Controller
 {
     public function index()
     {
+        $search = request()->query('search');
+        if ($search){
+            $posts = Post::where('title', 'LIKE', "%{$search}%")->simplePaginate(2);
+        } else {
+            $posts = Post::simplePaginate(2);
+        }
+
         return view('welcome')
             ->with('categories', Category::all())
             ->with('tags', Tag::all())
-            ->with('posts', Post::simplePaginate(2));
+            ->with('posts', $posts);
     }
 }
